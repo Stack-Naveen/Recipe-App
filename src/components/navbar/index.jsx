1b9fee898c88816ae9ffce623619fc7a0b4b2240
Talupula Naveen <stylistar05@gmail.com>
2025-09-22 23:06:10 +0530

    Implement NavBar component along with css

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,7 +1,37 @@
-export default function NavBar(){
-    return (
-        <div>
-            NavBar
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <h2 className="logo">
+          <NavLink to={"/"} className="link">
+            Recipe App
+          </NavLink>
+        </h2>
+
+        <div className="search-bar">
+          <input
+            type="text"
+            name="search"
+            placeholder="Enter a recipe name..."
+            className="search-input"
+          />
+          <button className="search-btn">Search</button>
         </div>
-    )
+
+        <div className="links">
+          <NavLink to={"/"} className="link">
+            Home
+          </NavLink>
+          <NavLink to={"/item/:id"} className="link">
+            Details
+          </NavLink>
+          <NavLink to={"/favourites"} className="link">
+            Favourites
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
 }
