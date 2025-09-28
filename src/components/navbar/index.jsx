a0125c21da884f3ef26b1c91098c9099d882ff8e
Talupula Naveen <stylistar05@gmail.com>
2025-09-28 16:31:54 +0530

    Create useContext for easy access

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,9 @@
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
 
 export default function NavBar() {
+  const { search, setSearch } = useContext(GlobalContext);
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -16,6 +19,8 @@ export default function NavBar() {
             name="search"
             placeholder="Enter a recipe name..."
             className="search-input"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
           />
           <button className="search-btn">Search</button>
         </div>
@@ -23,9 +28,6 @@ export default function NavBar() {
         <div className="links">
           <NavLink to={"/"} className="link">
             Home
-          </NavLink>
-          <NavLink to={"/item/:id"} className="link">
-            Details
           </NavLink>
           <NavLink to={"/favourites"} className="link">
             Favourites
---
+++ src/context/index.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+export const GlobalContext = createContext(null);
+
+export default function GlobalState({ children }) {
+  const [search, setSearch] = useState("");
+  console.log(search);
+  return (
+    <GlobalContext.Provider value={{ search, setSearch }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
